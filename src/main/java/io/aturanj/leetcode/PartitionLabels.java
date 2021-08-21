1847bdc50b0f3c89000a12da1fb40a69c3a0da08
a.turan <aturan@tuta.io>
2021-08-21 23:32:09 +0100

    added solution of Partition Labels

---
+++ src/main/java/io/aturanj/leetcode/PartitionLabels.java
@@ -0,0 +1,43 @@
+package io.aturanj.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * https://leetcode.com/problems/partition-labels/
+ */
+public class PartitionLabels {
+
+    public List<Integer> partitionLabels(String s) {
+
+        var maxIndex = s.lastIndexOf(s.charAt(0) + "");
+        var list = new ArrayList<String>();
+        var i = 0;
+        var startCharIndex = 0;
+
+        while (i < s.length()) {
+
+            for (; i <= maxIndex; i++) {
+                maxIndex = Math.max(maxIndex, s.lastIndexOf(s.charAt(i) + ""));
+            }
+
+            var part = s.substring(startCharIndex, maxIndex + 1);
+
+            list.add(part);
+
+            i = startCharIndex = maxIndex + 1;
+            if (i < s.length() - 1) {
+                maxIndex = s.lastIndexOf(s.charAt(maxIndex + 1) + "");
+            }
+        }
+
+        return list.stream().map(x -> x.length()).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        var partitionLabels = new PartitionLabels();
+        var result = partitionLabels.partitionLabels("ababcbacadefegdehijhklij");
+        result.forEach(x -> System.out.println(x));
+    }
+}
