b1f7d271f51ff4cf4b4fe021592dfdb7e9418822
a.turan <aturan@tuta.io>
2022-08-21 08:30:59 +0100

    updated solution of Length of Last Word

--- src/main/java/io/aturanj/leetcode/LengthOfLastWord.java
+++ src/main/java/io/aturanj/leetcode/LengthOfLastWord.java
@@ -1,5 +1,7 @@
 package io.aturanj.leetcode;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * https://leetcode.com/problems/length-of-last-word/
  */
@@ -12,7 +14,9 @@ public class LengthOfLastWord {
 
     public static void main(String[] args) {
         var lengthOfLastWord = new LengthOfLastWord();
-        var result = lengthOfLastWord.lengthOfLastWord("   fly me   to   the moon  ");
+        var result = lengthOfLastWord
+                .lengthOfLastWord("   fly me   to   the moon  ");
         System.out.println("result = " + result);
+        assertEquals(4, result);
     }
 }
